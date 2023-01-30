69d6b10cc77a0bfd494b09fdf117e8403ad3e6d5
Nathan Anderson <SniperSmiley@gmail.com>
2023-01-29 19:31:01 -0800

    a few days of work
    started by printing anything that follows << still need the value of the variable then I'll start on functions

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,6 +1,26 @@
+use pcre2::bytes::Regex;
 /// lexer module reads to the next syntax change
+struct pattern{
+    name: String,
+    match_: Regex,
+    begin: Regex,
+    end: Regex
+}
+
 pub fn lex(code: String){
-    for c in code.chars(){
-        println!("{}",c);
+    //let patterns = vec![];
+    for line in code.lines(){
+        let re = Regex::new(r"<<(.*?(?=<<|\Z|\||;))").unwrap();
+        let mut captured = false;
+        for result in re.captures_iter(&line.as_bytes()){
+            let t = result.unwrap();
+            let m = t.get(1).unwrap();
+            let s = m.as_bytes().to_vec().into_iter().map(|x| x as char).collect::<String>();
+            print!("{} ", s);
+            captured = true;
+        }
+        if captured{
+            println!();
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod collection;
 mod lexer;
 fn main() {
     /// morph module define syntax changing tools in the morph language
-    let code = collection::read_file("src\\test.mph");
+    let code = collection::read_file("src\\example.mph");
     lexer::lex(code);
     /// syntax update module updates the syntax
     /// parser module takes the tokens and parses them into a tree
