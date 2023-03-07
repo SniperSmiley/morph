cdc9449d9d6e8d88ea06e3c41a9bbc34a6c21a85
Nathan Anderson <SniperSmiley@gmail.com>
2023-03-06 22:47:14 -0800

    Working on Lexer
    starting the lexer using a shrinking input and matching only if on the first character; ^,regex
    plan to make a second for matching single characters

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,26 +1,86 @@
 use pcre2::bytes::Regex;
 /// lexer module reads to the next syntax change
-struct pattern{
-    name: String,
-    match_: Regex,
-    begin: Regex,
-    end: Regex
+pub struct Token{
+    pub name: String,
+    pub length: usize,
+    pub value: String,
 }
-
-pub fn lex(code: String){
-    //let patterns = vec![];
-    for line in code.lines(){
-        let re = Regex::new(r"<<(.*?(?=<<|\Z|\||;))").unwrap();
-        let mut captured = false;
-        for result in re.captures_iter(&line.as_bytes()){
-            let t = result.unwrap();
-            let m = t.get(1).unwrap();
-            let s = m.as_bytes().to_vec().into_iter().map(|x| x as char).collect::<String>();
-            print!("{} ", s);
-            captured = true;
+impl Token{
+    pub fn len(&self) -> usize{
+        return self.length.clone();
+    }
+}
+struct TokenType{
+    name: String,
+    regex: Regex,
+}
+impl TokenType{
+    pub fn capture(&self, window: &str) -> Option<Token>{
+        if let Some(capture) = self.regex.captures(window.as_bytes()).unwrap(){
+            if true{
+                return Some(Token{
+                    name: self.name.clone(),
+                    length: capture.get(0).unwrap().end() - capture.get(0).unwrap().start(),
+                    value: String::from_utf8(capture.get(0).unwrap().as_bytes().to_vec()).unwrap(),
+                });
+            }
         }
-        if captured{
-            println!();
+        return None;
+    }
+}
+pub struct Lexer{
+    code: String,
+    location: usize,
+    pub pending: Vec<Token>,
+    token_types: Vec<TokenType>,
+}
+impl Lexer{
+    pub fn new(code: String) -> Lexer{
+        return Lexer{
+            code: code,
+            location: 0,
+            pending: Vec::new(),
+            token_types: Vec::new(),
+        };
+    }
+    pub fn add_token_type(&mut self, name: String, regex: String){
+        self.token_types.push(TokenType{
+            name: name,
+            regex: Regex::new(regex.as_str()).unwrap(),
+        });
+    }
+    pub fn window(&self) -> String{
+        return self.code[self.location..].to_string();
+    }
+
+    pub fn consume_character(&mut self){
+        let mut potential:Token = Token{
+            name: "unknown".to_string(),
+            length: 0,
+            value: "".to_string(),
+        };
+        for token_type in &self.token_types{
+            if let Some(token) = token_type.capture(self.window().as_str()){
+                if token.len() > potential.len(){
+                    potential = token;
+                }
+            }
+        }
+        if potential.len() > 0{
+            self.location += potential.len();
+            self.pending.push(potential);
+        }else{
+            self.pending.push(Token{
+                name: "unknown".to_string(),
+                length: 1,
+                value: self.code[self.location..self.location+1].to_string(),
+            });
+            self.location += 1;
+        }
+    }
+    pub fn lex(&mut self){
+        while self.location < self.code.len(){
+            self.consume_character();
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
+//include the Lexer
+use lexer::Lexer;
 // This is the main file for the morph language
 mod collection;
 mod lexer;
 fn main() {
     /// morph module define syntax changing tools in the morph language
     let code = collection::read_file("src\\example.mph");
-    lexer::lex(code);
+    let mut lexer = Lexer::new(code);
+    lexer.add_token_type("string".to_string(), r#"^"[^"]*""#.to_string());
+    lexer.add_token_type("number".to_string(), r"^[0-9][0-9_]*".to_string());
+    lexer.add_token_type("identifier".to_string(), r"^[a-zA-Z_][a-zA-Z0-9_]*".to_string());
+    lexer.add_token_type("print".to_string(), r"^<<".to_string());
+    lexer.add_token_type("whitespace".to_string(), r"^\s+".to_string());
+    lexer.lex();
+    for i in lexer.pending{
+        println!("{}: {} {}", i.name, i.length, i.value);
+    }
     /// syntax update module updates the syntax
     /// parser module takes the tokens and parses them into a tree
     /// interpreter module takes the tree and interprets it
