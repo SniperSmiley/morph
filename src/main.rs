82d7e8551dd26c103ad9b5584a77d96fd0d68d38
Nathan Anderson <SniperSmiley@gmail.com>
2023-04-04 20:31:41 -0700

    tiniest change
    I was all ready to change to returning ast nodes but my steam faultered

--- src/main.rs
+++ src/main.rs
@@ -317,7 +317,7 @@ fn main() {
         rhs("Subtract",vec![def(vec![lhs("Sum"),key("-"),lhs("Product")])]),
         rhs("Multiply",vec![def(vec![lhs("Product"),key("*"),lhs("Atomic")])]),
         rhs("Divide",  vec![def(vec![lhs("Product"),key("/"),lhs("Atomic")])]),
-        rhs("Number",  vec![def(vec![opt(vec![key("-")]),arg(vec![lhs("Numeral")]),opt(vec![key("."),arg(vec![lhs("Numeral")])]),])]),
+        rhs("Number",  vec![cap("number",vec![opt(vec![key("-")]),arg(vec![lhs("Numeral")]),opt(vec![key("."),arg(vec![lhs("Numeral")])]),])]),
         rhs("Numeral", vec![cho(vec![key("1"),key("2"),key("3"),key("4"),key("5"),key("6"),key("7"),key("8"),key("9"),key("0"),])]),
     ];
     let test_equation = "-1.5+2*3+1/5-5/11".to_string();
